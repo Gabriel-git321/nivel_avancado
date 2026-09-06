166ce17630e5ce1cd15df097b2943f32b726e036
oliveiragabriel-eng <bielengsoftware@gmail.com>
2026-09-06 10:31:28 -0300

    feat: adiciona mensagem de Gabriel

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 def main():
     print("Projeto da atividade de Design Profissional")
     print("Fluxo colaborativo com Git e GitHub")
+    print("Alteração realizada por Gabriel")
 
 
 if __name__ == "__main__":
